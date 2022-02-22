ba8c0644d3733edac979f5a5b1523e2208ea5d25
Vlad Arsenjtev <Vlad.arsenjtev@gmail.com>
2022-02-22 17:07:28 +0200

    Task_2

--- src/com/vladarsenjtev/Main.java
+++ src/com/vladarsenjtev/Main.java
@@ -1,8 +1,26 @@
 package com.vladarsenjtev;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
+        Random random = new Random();
+        int[] array = new int[9];
 
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100) - 40;
+        }
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(square(array)));
+
+    }
+    public static int[] square(int[] array) {
+        int[] arrayOne = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            arrayOne[i] = (int) Math.pow(array[i], 2);
+        }
+        return arrayOne;
     }
 }
